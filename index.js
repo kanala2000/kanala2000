b9355002155e387ae7d75fd2962091bb762e5b4d
kanala2000 <91935677+kanala2000@users.noreply.github.com>
2026-02-08 04:16:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const AgentRequest = require("./agentrequest");
+const AgentRequest = require("./models/AgentRequest");
 
 const app = express();
 app.use(express.json());
