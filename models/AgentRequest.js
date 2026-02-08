6a5f99ca35c5bac677002f10dfe312661913c979
kanala2000 <91935677+kanala2000@users.noreply.github.com>
2026-02-08 05:38:40 +0530

    Update AgentRequest model

--- models/AgentRequest.js
+++ models/AgentRequest.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
 
-const AgentRequestSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const AgentRequestSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    mobile: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      default: "Pending"
+    },
+    agentId: {
+      type: String
+    },
+    password: {
+      type: String
+    }
   },
-  mobile: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    default: "Pending"
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("AgentRequest", AgentRequestSchema);
